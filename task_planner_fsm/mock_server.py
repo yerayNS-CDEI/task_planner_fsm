f4de93365420b180927730af77bb4c099f909956
Zewdie Habtie <zewdiehabtie26@gmail.com>
2026-03-12 10:12:57 +0100

    Add usage instructions for service calls and topic publishing.

--- task_planner_fsm/mock_server.py
+++ task_planner_fsm/mock_server.py
@@ -8,12 +8,15 @@ class MockServer(Node):
         super().__init__('mock_server')
         self.srv = self.create_service(SetBool, '/start_mapping', self.handle_request_mapping)
         self.get_logger().info("Server /start_mapping ready.")
+        self.get_logger().info("\033[1;32mUse: ros2 service call /start_mapping example_interfaces/srv/SetBool \"{data: true}\"\033[0m")
 
         self.srv = self.create_service(SetBool, '/start_geometry_reconstruction', self.handle_request_geometry_reconstruction)
         self.get_logger().info("Server /start_geometry_reconstruction ready.")
+        self.get_logger().info("\033[1;32mUse: ros2 service call /start_geometry_reconstruction example_interfaces/srv/SetBool \"{data: true}\"\033[0m")
 
         self.srv = self.create_service(SetBool, '/compute_areas_of_interest', self.handle_request_areas_of_interest)
         self.get_logger().info("Server /compute_areas_of_interest ready.")
+        self.get_logger().info("\033[1;32mUse: ros2 service call /compute_areas_of_interest example_interfaces/srv/SetBool \"{data: true}\"\033[0m")
 
     def handle_request_mapping(self, request, response):
         self.get_logger().info("Received request to start mapping.")
